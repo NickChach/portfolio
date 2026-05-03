abc14c5c1dbe2284f82dd9d5646b10ac7045274b
NickChach <debian5user@gmail.com>
2026-05-03 12:56:30 +0300

    vercel analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,83 +1,88 @@
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const IbmPlexSans = IBM_Plex_Sans({
-  weight: ["300", "400", "700"],
-  subsets: ["latin", "greek"]
-})
+	weight: ["300", "400", "700"],
+	subsets: ["latin", "greek"],
+});
 
 export const metadata = {
-  title: "NovaConstant — NickChachlioutis",
-  description: "Ανάπτυξη ιστοσελίδων για τον 21ο αιώνα. Υψηλού επιπέδου διαδικτυακές υπηρεσίες προσαρμοσμένες στις ανάγκες και το brand σας.",
-  authors: [{ name: "NovaConstant — NickChachlioutis" }],
-  keywords: [
-    "web developer", 
-    "nick chachlioutis", 
-    "νίκος χαχλιούτης", 
-    "nova constant",
-    "nova constant digital",
-    "νέα σταθερά",
-    "web solutions",
-    "web design",
-    "ανάπτυξη ιστοσελίδων",
-    "κατασκευή ιστοσελίδων",
-    "ανάπτυξη λογισμικού",
-    "ανάπτυξη εφαρμογών",
-    "freelancer",
-    "ελεύθερος επαγγελματίας",
-    "software engineer",
-    "custom web solutions",
-    "web design",
-    "seo",
-    "search engine optimasition",
-    "αθήνα",
-    "athens",
-    "αττική",
-    "attica",
-    "ρέθυμνο",
-    "rethymno",
-    "κρήτη",
-    "crete",
-    "τηλεργασία",
-    "remote",
-    "next.js"
-  ],
-  openGraph: {
-    title: "NovaConstant — NickChachlioutis",
-    description: "Ανάπτυξη ιστοσελίδων για τον 21ο αιώνα.",
-    url: 'https://www.nickchachlioutis.gr/',
-    siteName: "NovaConstant — NickChachlioutis",
-    images: [
-      {
-        url: "/images/nickchachlioutis.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "el_GR",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-snippet": -1,
-      "max-image-preview": "large",
-      "max-video-preview": -1
-    }
-  },
-  alternates: {
-    canonical: "https://nickchachlioutis.gr"
-  }
+	title: "NovaConstant — NickChachlioutis",
+	description:
+		"Ανάπτυξη ιστοσελίδων για τον 21ο αιώνα. Υψηλού επιπέδου διαδικτυακές υπηρεσίες προσαρμοσμένες στις ανάγκες και το brand σας.",
+	authors: [{ name: "NovaConstant — NickChachlioutis" }],
+	keywords: [
+		"web developer",
+		"nick chachlioutis",
+		"νίκος χαχλιούτης",
+		"nova constant",
+		"nova constant digital",
+		"νέα σταθερά",
+		"web solutions",
+		"web design",
+		"ανάπτυξη ιστοσελίδων",
+		"κατασκευή ιστοσελίδων",
+		"ανάπτυξη λογισμικού",
+		"ανάπτυξη εφαρμογών",
+		"freelancer",
+		"ελεύθερος επαγγελματίας",
+		"software engineer",
+		"custom web solutions",
+		"web design",
+		"seo",
+		"search engine optimasition",
+		"αθήνα",
+		"athens",
+		"αττική",
+		"attica",
+		"ρέθυμνο",
+		"rethymno",
+		"κρήτη",
+		"crete",
+		"τηλεργασία",
+		"remote",
+		"next.js",
+	],
+	openGraph: {
+		title: "NovaConstant — NickChachlioutis",
+		description: "Ανάπτυξη ιστοσελίδων για τον 21ο αιώνα.",
+		url: "https://www.nickchachlioutis.gr/",
+		siteName: "NovaConstant — NickChachlioutis",
+		images: [
+			{
+				url: "/images/nickchachlioutis.png",
+				width: 1200,
+				height: 630,
+			},
+		],
+		locale: "el_GR",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-snippet": -1,
+			"max-image-preview": "large",
+			"max-video-preview": -1,
+		},
+	},
+	alternates: {
+		canonical: "https://nickchachlioutis.gr",
+	},
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="el" className={IbmPlexSans.className}>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="el" className={IbmPlexSans.className}>
+			<body>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
 }
